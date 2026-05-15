66a1a86cf00559614a691278fa2c1e23d6c05c9e
Sarah Lee <s.lee.closet@gmail.com>
2026-05-15 23:45:39 +0900

    Calendar 메뉴 1
      - habbits, todos 달력에 개수 표시

--- src/components/dashboard/calendar/calendar-view.tsx
+++ src/components/dashboard/calendar/calendar-view.tsx
@@ -4,68 +4,74 @@ import styles from '@/components/dashboard/calendar/calendar.module.css'
 
 import FullCalendar from '@fullcalendar/react';
 import dayGridPlugin from '@fullcalendar/daygrid';
+import useHabbitListContext from '@/hooks/useHabbitListContext';
+import useTodoListContext from '@/hooks/useTodoListContext';
 
 export default function CalendarPage() {
-    const habbits = [
-        {
-            id: 'HL-20260514-005',
-            content: '점심 이후 10분 산책',
-            updatedAt: dayjs().subtract(18, 'minutes').subtract(5, 'hour').toDate(),
-            status: true,
-        },
-        {
-            id: 'VL-20260514-004',
-            content: '아침에 물 한잔 마시기',
-            updatedAt: dayjs().subtract(41, 'minutes').subtract(3, 'hour').toDate(),
-            status: false,
-        },
-        {
-            id: 'VL-20260514-003',
-            content: '독서 30분',
-            updatedAt: dayjs().subtract(1, 'hour').toDate(),
-            status: true,
-        },
-    ];
+  const { contents: habbits } = useHabbitListContext();
+  const { contents: todos } = useTodoListContext();
 
-    /**
-     * 완료된 habit만 날짜별로 개수 집계
-     */
-    const grouped = habbits.reduce((acc, habbit) => {
-        if (!habbit.status) {
-            return acc;
-        }
+  // 완료된 habit만 날짜별로 개수 집계
+  const groupedHabbit = habbits.reduce((acc, habbit) => {
+    if (!habbit.done) {
+      return acc;
+    }
 
-        const date = dayjs(habbit.updatedAt).format('YYYY-MM-DD');
-        acc[date] = (acc[date] || 0) + 1;
-        return acc;
-    }, {} as Record<string, number>);
+    const date = dayjs(habbit.createdAt).format('YYYY-MM-DD');
+    acc[date] = (acc[date] || 0) + 1;
+    return acc;
+  }, {} as Record<string, number>);
 
-    const events = Object.entries(grouped).map(([date, count]) => ({
-        title: `${count}`,
-        date,
-    }));
+  const habbitEvents = Object.entries(groupedHabbit).map(([date, count]) => ({
+    title: `${count}`,
+    date,
+  }));
 
-    return (
-        <FullCalendar
-            plugins={[dayGridPlugin]}
-            initialView="dayGridMonth"
-            height="auto"
-            fixedWeekCount={false}
-            dayMaxEventRows={true}
-            events={events}
-            eventBackgroundColor="transparent"
-            eventBorderColor="transparent"
+  // 완료된 todos만 날짜별로 개수 집계
+  const groupedTodo = todos.reduce((acc, todo) => {
+    if (!todo.done) {
+      return acc;
+    }
 
-            eventContent={(eventInfo) => (
-                <div>
-                    <div className={styles.count} style={{ backgroundColor: "#ea88b0", }}>
-                        {eventInfo.event.title}
-                    </div>
-                    <div className={styles.count} style={{ backgroundColor: "#9088ea", }}>
-                        {eventInfo.event.title}
-                    </div>
-                </div>
-            )}
-        />
-    );
+    const date = dayjs(todo.createdAt).format('YYYY-MM-DD');
+    acc[date] = (acc[date] || 0) + 1;
+    return acc;
+  }, {} as Record<string, number>);
+
+  const todoEvents = Object.entries(groupedTodo).map(([date, count]) => ({
+    title: `${count}`,
+    date,
+  }));
+
+  const events = [
+    ...habbitEvents.map(e => ({ ...e, extendedProps: { type: 'habbit' } })),
+    ...todoEvents.map(e => ({ ...e, extendedProps: { type: 'todo' } }))
+  ];
+
+  return (
+    <FullCalendar
+      plugins={[dayGridPlugin]}
+      initialView="dayGridMonth"
+      height="auto"
+      fixedWeekCount={false}
+      dayMaxEventRows={true}
+      events={events}
+      eventBackgroundColor="transparent"
+      eventBorderColor="transparent"
+
+      eventContent={(eventInfo) => {
+        const type = eventInfo.event.extendedProps.type;
+        const isHabbit = type === 'habbit';
+
+        return (
+          <div
+            className={styles.count}
+            style={{ backgroundColor: isHabbit ? "#ea88b0" : "#9088ea" }}
+          >
+            {eventInfo.event.title}
+          </div>
+        );
+      }}
+    />
+  );
 }
